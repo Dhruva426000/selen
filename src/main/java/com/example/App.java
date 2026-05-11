1d3787fdee63466c76cdf3ded5e9859ec7a28f57
Dhruva426000 <dhruvanaganand@gmail.com>
2026-05-11 13:00:30 +0530

    Using system Chrome in Jenkins

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,31 +9,28 @@ public class App {
 
     public static void main(String[] args) {
 
-        // Chrome options for Jenkins/Linux
         ChromeOptions options = new ChromeOptions();
 
+        // Force system Chrome
+        options.setBinary("/usr/bin/google-chrome");
+
+        // Jenkins/Linux options
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
-        // Launch Chrome with options
         WebDriver driver = new ChromeDriver(options);
 
-        // Open website
         driver.get("https://www.saucedemo.com/");
 
-        // Login
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
 
-        // Print message
         System.out.println("Login Successful");
 
-        // Close browser
         driver.quit();
 
-        // Stop lingering threads
         System.exit(0);
     }
 }
