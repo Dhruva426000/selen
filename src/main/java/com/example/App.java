e50f22773f7c0923bc934c74a31b5aab91d3b110
Dhruva426000 <dhruvanaganand@gmail.com>
2026-05-11 12:57:06 +0530

    Updated Jenkins headless code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,38 +3,37 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
 
     public static void main(String[] args) {
 
-        // Launch Chrome normally
-        WebDriver driver = new ChromeDriver();
+        // Chrome options for Jenkins/Linux
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        // Launch Chrome with options
+        WebDriver driver = new ChromeDriver(options);
 
         // Open website
         driver.get("https://www.saucedemo.com/");
-
-        // Maximize window
-        driver.manage().window().maximize();
 
         // Login
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
 
-        // Success message
+        // Print message
         System.out.println("Login Successful");
-
-        // Wait 3 seconds
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         // Close browser
         driver.quit();
 
+        // Stop lingering threads
         System.exit(0);
     }
 }
