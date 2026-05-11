58b4280343ff950170ad97e675d117877cd07d68
Dhruva426000 <dhruvanaganand@gmail.com>
2026-05-11 14:23:42 +0530

    Updated Selenium code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,36 +3,28 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.io.File;
 
 public class App {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-
         ChromeOptions options = new ChromeOptions();
 
-        options.setBinary("/usr/bin/google-chrome");
-
-        // Important for Jenkins/Linux
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--remote-debugging-port=9222");
 
-        ChromeDriverService service = new ChromeDriverService.Builder()
-                .usingDriverExecutable(new File("/usr/local/bin/chromedriver"))
-                .build();
+        WebDriver driver = new ChromeDriver(options);
 
-        WebDriver driver = new ChromeDriver(service, options);
-
+        // Open SauceDemo
         driver.get("https://www.saucedemo.com/");
 
+        // Print page details
+        System.out.println("Title: " + driver.getTitle());
+        System.out.println("URL: " + driver.getCurrentUrl());
+
+        // Login
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
